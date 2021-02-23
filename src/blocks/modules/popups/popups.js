1f8564d61396089b3bbfbf406fa9d035536ee4a1
Pavel <pavelfedorov929@gmail.com>
2021-02-23 18:35:56 +0300

    Попап с входом: есть баг

--- src/blocks/modules/popups/popups.js
+++ src/blocks/modules/popups/popups.js
@@ -1,13 +1,40 @@
-$('.popup__close-btn').on('click', function () {
-    $('.overlay, .popup').fadeOut('slow');
+$(".popup__close-btn").on("click", function () {
+  $(".overlay, .popup").fadeOut("slow");
 });
 
-$('.filter-style').styler();
+$(".filter-style").styler();
 
-$('input[name=phone]').mask("+375 (99) 999-99-99");
+$("input[name=phone]").mask("+375 (99) 999-99-99");
 
-$('#email-enter').on('click', function () {
-    $(this).css('display', 'none');
-    $('.enter-popup__phone-enter').css('display', 'none');
-    $('.enter-popup__email-enter').css('display', 'block');
+$("#email-enter").on("click", function (e) {
+  e.preventDefault();
+  $(this)
+    .children()
+    .text(
+      $(this).text() == "Войти через email"
+        ? "Войти по номеру телефона"
+        : "Войти через email"
+    );
+  $(".enter-popup__phone-enter").toggleClass("enter-popup__phone-enter--hide");
+  $(".enter-popup__email-enter").toggleClass("enter-popup__email-enter--show");
 });
+
+/* @@include("../blocks/modules/hamburger/hamburger.js")
+
+  @@include("tabs.js")
+
+  @@include("../blocks/modules/directions/directions.js")
+
+  @@include("../blocks/modules/calculator/calculator.js")
+
+  @@include("../blocks/modules/clients/clients.js")
+
+  @@include("../blocks/modules/media/media.js")
+
+  @@include("../blocks/modules/start/start.js")
+
+  @@include("../blocks/modules/questions/questions.js")
+
+  @@include("scroll.js")
+
+  @@include("../blocks/modules/popups/popups.js") */
--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,15 @@
 $(function () {
+    $(".popup__close-btn").on("click", function () {
+        $(".overlay, .popup").fadeOut("slow");
+    });
 
-    @@include('../blocks/modules/hamburger/hamburger.js')
+    $(".filter-style").styler();
 
-    @@include('tabs.js')
+    $("input[name=phone]").mask("+375 (99) 999-99-99");
 
-    @@include('../blocks/modules/directions/directions.js')
-
-    @@include('../blocks/modules/calculator/calculator.js')
-
-    @@include('../blocks/modules/clients/clients.js')
-
-    @@include('../blocks/modules/media/media.js')
-
-    @@include('../blocks/modules/start/start.js')
-
-    @@include('../blocks/modules/questions/questions.js')
-
-    @@include('../blocks/modules/popups/popups.js')
-
-    @@include('scroll.js')
-
+    $("#email-enter").on("click", function () {
+        $(this).children().text($(this).text() == "Войти через email" ? "Войти по номеру телефона" : "Войти через email");
+        $(".enter-popup__phone-enter").toggleClass("enter-popup__phone-enter--hide");
+        $(".enter-popup__email-enter").toggleClass("enter-popup__email-enter--show");
+    });
 });
