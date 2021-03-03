08f2c4f86f25cc3d8dd7934570d8cb26091a88d2
Pavel <pavelfedorov929@gmail.com>
2021-03-03 09:02:20 +0300

    Подключены скрипты

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,21 @@
 $(function () {
+
+    @@include('tabs.js')
+
+    @@include('../blocks/modules/hamburger/hamburger.js')
+
+    @@include('../blocks/modules/directions/directions.js')
+
+    @@include('../blocks/modules/calculator/calculator.js')
+
+    @@include('../blocks/modules/clients/clients.js')
+
+    @@include('../blocks/modules/media/media.js')
+
+    @@include('../blocks/modules/start/start.js')
+
+    @@include('../blocks/modules/questions/questions.js')
+
     $(".popup__close-btn").on("click", function () {
         $(".overlay, .popup").fadeOut("slow");
     });
